2484e334124e9e2bee63c94a552e6fd4a34001f4
Mateus Paiva <mateusopaiva@gmail.com>
2024-02-24 18:46:02 -0300

    Desenvolvimento da API - PT1

---
+++ api/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`🚀 Server is runing on http://localhost:${port}`);
+    });
+  })
+  .catch (() => console.log('Erro ao conectar no mongodb'));
