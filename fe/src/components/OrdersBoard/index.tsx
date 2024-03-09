14b7aaf6ac26fcfbea3cf8dc5158025ba6be0cc7
Mateus Paiva <mateusopaiva@gmail.com>
2024-03-09 19:15:37 -0300

    Desenvolvimento da UI Web com React e TS - PT1

---
+++ fe/src/components/OrdersBoard/index.tsx
@@ -0,0 +1,30 @@
+import { Order } from '../../types/Order';
+import { Board, OrdersContainer } from "./styled";
+
+interface OrdersBoardProps {
+  icon: string;
+  title: string;
+  orders: Order[];
+}
+
+export function OrdersBoard({ icon, title }: OrdersBoardProps) {
+  return (
+    <Board>
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>(1)</span>
+      </header>
+      <OrdersContainer>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+      </OrdersContainer>
+    </Board>
+  );
+}
