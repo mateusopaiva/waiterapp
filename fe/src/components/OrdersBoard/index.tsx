1740b4fc0a59342c5026951b3c3ba9d07994291b
Mateus Paiva <mateusopaiva@gmail.com>
2024-03-16 18:58:51 -0300

    Desenvolvimento da UI Web com React e TS - PT2

--- fe/src/components/OrdersBoard/index.tsx
+++ fe/src/components/OrdersBoard/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from "./styled";
 
 interface OrdersBoardProps {
@@ -7,24 +10,44 @@ interface OrdersBoardProps {
   orders: Order[];
 }
 
-export function OrdersBoard({ icon, title }: OrdersBoardProps) {
+export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
   return (
     <Board>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+        />
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
-      <OrdersContainer>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
+
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map((order) => (
+            <button type="button" key={order._id} onClick={() => handleOpenModal(order)}>
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
     </Board>
   );
 }
