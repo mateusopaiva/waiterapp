9801781b3bda249dbbee77fe10b6daa1983324f0
Mateus Paiva <mateusopaiva@gmail.com>
2024-03-30 21:56:28 -0300

    Integração do front-end web com a API + implementação dos WebSockets

--- fe/src/components/OrderModal/index.tsx
+++ fe/src/components/OrderModal/index.tsx
@@ -11,9 +11,12 @@ interface OrderModalProps {
   visible: boolean;
   order: Order | null;
   onClose: () => void;
+  onCancelOrder: () => Promise<void>;
+  isLoading: boolean;
+  onChangeOrderStatus: () => void;
 }
 
-export function OrderModal({ visible, order, onClose }: OrderModalProps) {
+export function OrderModal({ visible, order, onClose, onCancelOrder, isLoading, onChangeOrderStatus }: OrderModalProps) {
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
       if(event.key === 'Escape') {
@@ -90,12 +93,29 @@ export function OrderModal({ visible, order, onClose }: OrderModalProps) {
         </OrderDetails>
 
         <Actions>
-          <button type="button" className="primary">
-            <span>🧑‍🍳</span>
-            <strong>Iniciar Produção</strong>
-          </button>
+          {order.status !== 'DONE' && (
+            <button
+              type="button"
+              className="primary"
+              disabled={isLoading}
+              onClick={onChangeOrderStatus}
+            >
+            <span>
+              {order.status === 'WAITING' && '🧑‍🍳'}
+              {order.status === 'IN_PRODUCTION' && '✅'}
+            </span>
+            <strong>
+              {order.status === 'WAITING' && 'Iniciar Produção'}
+              {order.status === 'IN_PRODUCTION' && 'Concluir Pedido'}
+            </strong>
+            </button>
+          )}
 
-          <button type="button" className="secondary">
+          <button
+            type="button"
+            className="secondary"
+            onClick={onCancelOrder}
+          >
               Cancelar pedido
           </button>
         </Actions>
